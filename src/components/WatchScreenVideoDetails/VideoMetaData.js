f58cc7e1f798ff40416ce54c6f0f462908d03682
Sumukha <sumukahegde007@gmail.com>
2024-06-09 22:33:50 +0530

    optimization

--- src/components/WatchScreenVideoDetails/VideoMetaData.js
+++ src/components/WatchScreenVideoDetails/VideoMetaData.js
@@ -50,8 +50,6 @@ const VideoMetaData = ({ videoDetails, videoId }) => {
 
   useEffect(() => {
     getChannelDetailsById();
-
-    // getSubscriptionStatus();
   }, [videoId]);
   return (
     channelDetails && (
